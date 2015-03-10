d0dfb25428416d580425dc7f8ae425a4c2353473
Jaseem Abid <jaseemabid@gmail.com>
2015-03-11 02:15:54 +0530

    Tests with tasty
    Ref: http://documentup.com/feuerbach/tasty

--- test/Test.hs
+++ test/Test.hs
@@ -1,19 +1,26 @@
 module Main where
 
-import Test.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
 
 import Lisper
 import Lisper.Core
 import Lisper.Eval
 
+env :: Env
 env = [("one", Number 1)]
 
-testSimple = TestCase $ assertEqual
-  "Should get simple references" (Number 1) (resolve env "one")
+testSimple = testCase "Should get simple references"  $
+             resolve env "one" @?= Number 1
 
-testFail = TestCase $ assertEqual
-  "Should get simple references" (Number 42) (resolve env "one")
+testFail = testCase "Should fail for missing references" $
+           resolve env "t" @?= Number 1
 
--- Main
-main :: IO Counts
-main = runTestTT $ TestList [testSimple, testFail]
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [testSimple, testFail]
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+main :: IO ()
+main = defaultMain tests
