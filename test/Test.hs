ca9baae324c167f2c89eb798da16ae40dbc43ed2
Jaseem Abid <jaseemabid@gmail.com>
2015-12-13 14:42:57 +0530

    Build with stack

--- test/Test.hs
+++ test/Test.hs
@@ -3,23 +3,25 @@ module Main where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Lisper
 import Lisper.Core
 import Lisper.Eval
 
 env :: Env
 env = [("one", Number 1), ("ref", Atom "one"), ("t", Atom "two")]
 
+testSimple :: TestTree
 testSimple = testCase "Should get variables"  $
              resolve env "one" @?= Just (Number 1)
 
+testFail :: TestTree
 testFail = testCase "Should fail for missing variables" $
            resolve env "nope" @?= Nothing
-
+testRef :: TestTree
 testRef = testCase "Should get references" $
           resolve env "ref" @?= Just (Number 1)
 
 -- Should this be `Atom "two"` ?
+testBadRef :: TestTree
 testBadRef = testCase "Should fail for missing references" $
              resolve env "t" @?= Nothing
 
