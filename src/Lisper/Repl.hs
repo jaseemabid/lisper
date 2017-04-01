5b5de5d0c695098c502565a3004e9ce8f18b9bce
Jaseem Abid <jaseemabid@gmail.com>
2017-04-01 09:23:37 +0530

    Move the entire REPL into a State Monad
    Thanks Amar

--- src/Lisper/Repl.hs
+++ src/Lisper/Repl.hs
@@ -5,30 +5,34 @@ module Lisper.Repl (runRepl) where
 import Lisper.Core (Env)
 import Lisper.Eval
 
+import Control.Monad.State.Lazy
 import System.Console.Haskeline
 import System.Directory (getHomeDirectory)
 
 -- [todo] - Improve input from stdin, Ie echo "(+ 1 1)" | lisper
--- [TODO] - Move the entire REPL into a State Monad
 
 runRepl :: IO ()
 runRepl = do
     f <- (++ "/.lisper_history") <$> getHomeDirectory
     let settings = defaultSettings { historyFile = Just f}
-    runInputT settings $ loop []
+    runInputT settings (evalStateT loop [])
 
   where
-    loop :: MonadException m => Env -> InputT m ()
-    loop env =
-        getInputLine "λ " >>= \case
+    loop :: MonadException m => StateT Env (InputT m) ()
+    loop =
+        lift (getInputLine "λ ") >>= \case
             Nothing  -> return ()
             Just "q" -> return ()
-            Just "" -> loop env
-            Just line ->
+            Just "" -> loop
+            Just line -> do
+                env <- get
+
                 case run env line of
                   (Right result, env') -> do
-                      outputStrLn $ show result
-                      loop env'
-                  (Left err, _) -> do
-                      outputStrLn err
-                      loop env
+                      put env'
+                      lift $ outputStrLn (show result)
+
+                  (Left err, _) ->
+                      lift $ outputStrLn err
+
+                loop
