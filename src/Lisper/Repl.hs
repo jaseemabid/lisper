jaseemabid/lisper

commit 5b5de5d0c695098c502565a3004e9ce8f18b9bce

Move the entire REPL into a State Monad
Thanks Amar

src/Lisper/Repl.hs
@@ -5,30 +5,34 @@ module Lisper.Repl (runRepl) where
 import Lisper.Core (Env)
 import Lisper.Eval
 
+import Control.Monad.State.Lazy
 import System.Console.Haskeline
 import System.Directory (getHomeDirectory)
 
 -- [todo] - Improve input from stdin, Ie echo "(+ 1 1)" | lisper
--- [TODO] - Move the entire REPL into a State Monad
 
 runRepl :: IO ()
 runRepl = do
     f <- (++ "/.lisper_history") <$> getHomeDirectory
     let settings = defaultSettings { historyFile = Just f}
-    runInputT settings $ loop []
+    runInputT settings (evalStateT loop [])
 
   where
-    loop :: MonadException m => Env -> InputT m ()
-    loop env =
-        getInputLine "λ " >>= \case
+    loop :: MonadException m => StateT Env (InputT m) ()
+    loop =
+        lift (getInputLine "λ ") >>= \case
             Nothing  -> return ()
             Just "q" -> return ()
-            Just "" -> loop env
-            Just line ->
+            Just "" -> loop
+            Just line -> do
+                env <- get
+
                 case run env line of
                   (Right result, env') -> do
-                      outputStrLn $ show result
-                      loop env'
-                  (Left err, _) -> do
-                      outputStrLn err
-                      loop env
+                      put env'
+                      lift $ outputStrLn (show result)
+
+                  (Left err, _) ->
+                      lift $ outputStrLn err
+
+                loop

commit 1467e1306aa1e540335c832be4c6cf9fe99a6f92

REPL is broken since 5b5de5d

src/Lisper/Repl.hs
@@ -28,6 +28,8 @@ runRepl = do
             Just line -> do
                 case read line of
                     Right ast ->
+                      -- [TODO] - REPL is broken. Make evaluate use the state.
+                      -- Cause 5b5de5d
                       case evaluate ast of
                           (Right (result, env')) -> do
                               put env'